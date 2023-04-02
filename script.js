2cfcc8653d7199ed05af1b93fe6d6e8d3b5ad48f
mehran vejdani <mehran.vejdanii@gmail.com>
2023-04-02 19:58:08 +0430

    add trash icon and fixed all

--- script.js
+++ script.js
@@ -15,11 +15,22 @@ function addItem() {
   let trashIcon = document.createElement("i");
   divParent.className = "item";
   divParent.innerHTML = `<div>` + input.value + `</div>`;
-  checkIcon.classList = "fas fa-check-square";
-  checkIcon.style.color = "lightgray";
+  checkIcon.classList = "fas fa-check-square check";
+  //   checkIcon.style.color = "lightgray";
   ///checkIcon event
   checkIcon.addEventListener("click", () => {
-    checkIcon.style.color = "green";
+    checkIcon ? checkIcon.classList.toggle("green") : "";
   });
   divChild.appendChild(checkIcon);
+
+  //trash the event
+  trashIcon.className = "fas fa-trash";
+  trashIcon.style.color = "darkgray";
+  trashIcon.addEventListener("click", () => {
+    divParent.remove();
+  });
+  divChild.appendChild(trashIcon);
+  divParent.appendChild(divChild);
+  toDoItems.appendChild(divParent);
+  input.value = "";
 }
