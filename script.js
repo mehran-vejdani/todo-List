a83b6f31e5c630d2d856680ac2340dd4556c978d
mehran vejdani <mehran.vejdanii@gmail.com>
2023-04-02 19:06:30 +0430

    add function to make todo list with checkbox

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 const toDoItems = document.querySelector(".to-do-items");
 const input = document.querySelector(".input");
 const trashIcon = document.querySelector(".trash");
+
+input.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    addItem();
+  }
+});
+
+function addItem() {
+  let divParent = document.createElement("div");
+  let divChild = document.createElement("div");
+  let checkIcon = document.createElement("i");
+  let trashIcon = document.createElement("i");
+  divParent.className = "item";
+  divParent.innerHTML = `<div>` + input.value + `</div>`;
+  checkIcon.classList = "fas fa-check-square";
+  checkIcon.style.color = "lightgray";
+  ///checkIcon event
+  checkIcon.addEventListener("click", () => {
+    checkIcon.style.color = "green";
+  });
+  divChild.appendChild(checkIcon);
+}
